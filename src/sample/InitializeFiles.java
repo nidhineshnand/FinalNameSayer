de3583ec0586df0f51357cce974fc05d88ea9720
nidhineshnand <nnan773@aucklanduni.ac.nz>
2018-10-01 19:55:44 +1300

    Added feature where database files get copied in the resources directory

--- src/sample/InitializeFiles.java
+++ src/sample/InitializeFiles.java
@@ -2,6 +2,7 @@ package sample;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 /**This class initializes the files that will be used by this program in the future*/
 public class InitializeFiles {
@@ -26,8 +27,28 @@ public class InitializeFiles {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        copyDatabaseFiles();
     }
 
+    //This method copies the files from where the user specifies to our internal folder
+    private void copyDatabaseFiles() {
+        //Reading database files
+        File databaseFolder = new File(_databaseFilePath);
+        File[] listOfFiles = databaseFolder.listFiles();
 
+        for (File file : listOfFiles) {
+            //If file exists then it is not copied
+            if (!new File("./resources/database/" + file.getName()).exists()) {
+
+                //Copying database recording file
+                try {
+                    Files.copy(file.toPath(), (new File("./resources/database/" + file.getName()).toPath()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
 }
