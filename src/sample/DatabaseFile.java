86a5507227c0ce64b8cfdd54a820e6d7cb9cd1be
nidhineshnand <nnan773@aucklanduni.ac.nz>
2018-10-01 17:46:41 +1300

    Set up file structure

---
+++ src/sample/DatabaseFile.java
@@ -0,0 +1,11 @@
+package sample;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class DatabaseFile implements NameSayerFile {
+    @Override
+    public ArrayList<File> getFilesToPlay() {
+        return null;
+    }
+}
---
+++ src/sample/NameSayerFile.java
@@ -0,0 +1,9 @@
+package sample;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public interface NameSayerFile {
+
+    public ArrayList<File> getFilesToPlay();
+}
