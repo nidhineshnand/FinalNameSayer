b2457a8a840fc20b0c5ebd732b5c39305238a4d1
nidhineshnand <nnan773@aucklanduni.ac.nz>
2018-10-11 16:42:54 +1300

    Added CollectionsFile. This class will be used to play multiple recordings

---
+++ src/sample/CollectionsFile.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.scene.layout.Pane;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class CollectionsFile implements NameSayerFile {
+
+
+    @Override
+    public ArrayList<File> filesToPlay() {
+        return null;
+    }
+
+    @Override
+    public String get_displayName() {
+        return null;
+    }
+
+    @Override
+    public Pane get_fileView() {
+        return null;
+    }
+}
