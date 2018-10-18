c142ce242771a7ce7e6dcb79fdb50f6d8c946167
MelancholicGhost <jkim584@aucklanduni.ac.nz>
2018-10-19 11:34:28 +1300

    template controller for shop scene is created

--- src/application/ShopSceneController.java
+++ src/application/ShopSceneController.java
@@ -1,5 +1,29 @@
 package application;
 
-public class ShopSceneController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
+public class ShopSceneController extends Controller {
+	
+	@FXML
+	public Button _defaultThemeButton;
+	@FXML
+	public Button _darkThemeButton;
+	
+	/**
+	 * When _dafaultThemeButton is clicked
+	 */
+	@FXML
+	void defaultThemeClicked() {
+		
+	}
+	
+	/**
+	 * When _darkThemeButton is clicked
+	 */
+	@FXML
+	void darkThemeClicked() {
+		
+	}
+	
 }
