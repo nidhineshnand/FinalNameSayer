b947b38fd7272fedd341b9228fa11a89d788232a
nidhineshnand <nnan773@aucklanduni.ac.nz>
2018-10-20 15:13:24 +1300

    Added save points method in points class

--- src/sample/PointsSystem.java
+++ src/sample/PointsSystem.java
@@ -1,6 +1,17 @@
 package sample;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class PointsSystem {
+
+    String _pathToSave;
+
+    /**This class manages the points of the whole system*/
+    PointsSystem(String pathToSave){
+        _pathToSave = pathToSave;
+    }
 
     private int _points;
 
@@ -10,5 +21,19 @@ public class PointsSystem {
 
     public int get_points() {
         return _points;
+    }
+
+    //Saves the current point to a new class
+    public void savePoints(){
+        File file = new File(_pathToSave);
+
+        //Writing to file
+        try {
+            FileWriter f2 = new FileWriter(file, false);
+            f2.write(_points);
+            f2.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
